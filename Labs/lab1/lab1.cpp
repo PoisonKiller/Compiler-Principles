93075fd8c8e3067653662b2ab08b643f63d9ac20
Chen Jintao <74226176+PoisonKiller@users.noreply.github.com>
2023-04-15 12:15:47 +0800

    Add files via upload

---
+++ Labs/lab1/lab1.cpp
@@ -0,0 +1,205 @@
+﻿/*
+改写之后的文法：
+expr -> term  rest | (expr)
+rest -> + term rest | - term rest | epsilon 
+term -> num | id | num.num | num.E(+|-)num
+
+输入样例：
+10 + 20 - a + b+temp
+输出样例： 
+10 20 + a - b + temp +
+
+需要你补充完成的：
+1. gettoken()的功能，识别浮点数
+2. *、/运算及配对括号
+ 
+*/
+
+#include<stdio.h> 
+#include<ctype.h>
+#include<stack>
+
+#define FLOAT 550 
+#define NUM 500
+#define ID 600
+#define E 1321
+#define E1 231
+
+
+
+//全局量<tokenname, tokenvalue>  
+int lookahead;   		//存放当前的词法单元的类别，即tokenname 
+int tokenval = NULL;	//
+char lexeme[1024];		//当前的词法单元对应的词素（字符串 ） 
+int pretoken = NULL;
+
+
+void expr(); 
+void term();
+void rest();
+void factor();
+void puttoken(int token);
+int gettoken(); 
+
+int gettoken()			//词法分析函数，返回值：当前词法单元的类型 
+{
+	char t;
+	int i; 
+	
+	while (1) {
+		t = getchar();
+		if (t == ' ' || t == '\t' ) // 空格或者换行符->过滤掉 
+			;
+		else if (t == '\n') { // 因为在本程序中，回车符作为表达式的结束，
+			return t;
+		} 	
+								// 所以不能把回车符当成空白符过滤 
+		else if (isdigit(t)) {		//识别整型数值常量 
+			tokenval = 0;
+			while (isdigit(t)) {
+				tokenval = tokenval * 10 + t - '0';
+				t = getchar();
+			}
+			if (t == '.') return FLOAT; // 识别到小数点 
+			if (t == 'E') {				// 识别到指数 
+				t = getchar();
+				if(t == '-') return E1;
+				ungetc(t, stdin);
+				return E;
+			}
+			ungetc(t, stdin);
+			return NUM;
+		}
+		else if( isalpha(t)) {		//识别标识符 
+             i=0; 
+             do {
+                 lexeme[i++]=t; t = getchar(); 
+             }while( isalpha(t) || isdigit(t) );
+             lexeme[i]='\0'; 
+			 ungetc(t, stdin);
+             return ID;
+         }
+		else {			//识别其他单个字符，例如 + - * / 的ASCII值作为tokenname
+			tokenval = NULL;
+			return t;
+		}
+	}
+}
+
+void puttoken(int token)
+{
+	if (token == NUM)
+		printf("%d ", tokenval);
+	else if (token == FLOAT)
+		printf("%d.", tokenval);
+	else if (token == ID)
+		printf("%s ", lexeme);
+	else if (token == E)
+		printf("%dE", tokenval);
+	else if (token == E1)
+		printf("%dE-", tokenval);		
+	else
+		printf("%c ", (char)token);				
+}
+ 
+void error(char *msg)
+{
+	printf("%s\n", msg);
+}
+
+void match(int token)
+{
+	if (lookahead == token) 
+		lookahead = gettoken(); 
+	else
+		error("syntax error");
+}
+
+/*
+expr -> term rest 
+rest -> + term rest | - term rest | epsilon 
+term -> num | id 
+*/
+void expr()
+{
+	factor();
+    term();  //先执行term，消除左递归
+	rest();
+}
+
+void rest( )
+{
+    if (lookahead == '+')
+    {
+        match('+');
+		factor();
+        term();
+        puttoken('+');
+		rest();
+    }
+    if (lookahead == '-')
+    {
+        match('-');
+		factor();
+        term();
+        puttoken('-');
+		rest();
+    }
+    else ;
+}
+
+void term( )
+{
+   	if (lookahead == '*') {
+   		match('*');  
+		factor();
+		puttoken('*');
+		term();
+	}
+   	else if (lookahead == '/') {
+   		match('/');   
+		factor();
+		puttoken('/'); 
+		term();
+	}
+   	else ;
+}
+
+void factor()
+{
+	if (lookahead == NUM) {
+		puttoken(lookahead); match(NUM); 
+	}
+	else if (lookahead == FLOAT) {
+		puttoken(lookahead); match(FLOAT);
+		factor();
+	}
+	//浮点数包括两个factor 
+	else if (lookahead == E) {
+		puttoken(lookahead); match(E);
+		factor(); 
+	}
+	else if (lookahead == E1) {
+		puttoken(lookahead); match(E1);
+		factor();
+	}
+	else if (lookahead == ID) {
+		puttoken(lookahead); match(ID);
+	}
+	else if (lookahead == '(') {
+		match('(');
+		expr();
+		match(')');
+	}
+    else error("syntax error");
+}
+// (10+20)*a-temp 
+// (10+20.5)*a-temp
+// (10.2345+20.5E-10)*a-temp
+// a * (10.2345 + 20.5E-10)  - temp
+int main()
+{
+	lookahead = gettoken();
+	expr();
+	return 0;
+}
---
+++ Labs/lab3/lab2.cpp
@@ -0,0 +1,377 @@
+/*
+文法
+终结符： ID、NUM、IF、'{'、'}'  ';' '=', '<', '>', '<=', 
+        '>=', '==', '!=', '+', '-', '*', '/', '(', ')', 
+        'int', 'float', 'char', 'else', 'while', 'do', 'break'
+
+1 program -> block
+2 block -> '{' decls stmts '}'
+3 decls -> decl decls
+4        | epsilon
+5 decl -> type ID ';'
+
+5 stmts -> stmt stmts
+6       | epsilon
+
+7 stmt -> ID '=' expr ';'
+8		  | IF (bool) stmt ElseStmt
+9         | WHILE (bool) stmt
+10        | DO stmt WHILE (bool) ';'
+11        | break ';'
+12        | block
+
+13 ElseStmt -> ELSE stmt
+14          | epsilon
+
+15  bool -> expr boolR
+
+16  boolR -> < expr
+17        | > expr
+18        | <= expr
+19        | >= expr
+20        | == expr
+21        | != expr
+22        | epsilon
+
+23  expr -> term exprR
+
+24  exprR -> + term exprR
+25        | - term exprR
+26        | epsilon
+
+27  term -> factor termR
+
+28  termR -> * factor termR
+29        | / factor termR
+30        | epsilon
+
+31  factor -> (expr)| ID | NUM
+32  type -> INT | FLOAT | CHAR
+
+
+
+
+FIRST集
+program -> {
+block -> {
+decls -> int float char epsilon
+decl -> int float char
+stmts -> ID IF WHILE DO break { epsilon
+stmt -> ID IF WHILE DO break { 
+ElseStmt -> else epsilon
+bool -> ID NUM (
+boolR -> < > <= >= == != epsilon
+expr -> ID NUM (
+exprR -> + - epsilon
+term -> ID NUM (
+termR -> * / epsilon
+factor -> ID NUM (
+type -> int float char
+
+follow集
+program -> $
+block -> $ }
+decls -> ID IF WHILE DO break { }
+decl -> ID IF WHILE DO break { }
+stmts -> ID IF WHILE DO break { }
+stmt -> ID IF WHILE DO break { }
+ElseStmt -> ID IF WHILE DO break { }
+bool -> = } 
+boolR ->  } $
+expr -> = } $ ;
+exprR ->  } $ ;
+term -> = ) + - < > <= >= == != ; IF WHILE DO break { } $
+termR -> = } $ ;
+factor -> = } ; $
+type -> ID
+
+*/ 
+
+
+#include <iostream>
+#include <fstream>
+#include "lex.yy.c"    //由flex .l文件得到 
+using namespace std;
+
+TOKEN lookahead;
+
+void program();
+void block();
+void decls();
+void decl();
+void stmts();
+void stmt();
+void ElseStmt();
+void type();
+void Bool();
+void BoolR();
+void expr();
+void exprR();
+void term();
+void termR();
+void factor();
+void type();
+
+
+void error(int no)
+{
+	printf("error: %d\n", no);	
+} 
+
+void match(TOKEN t)
+{
+	if (lookahead == t)	
+		lookahead = yylex();
+	else
+		printf("token %d %c is needed\n", t, t);
+} 
+
+void program()
+{
+	printf("program -> block\n");
+	block();
+}
+
+void block()
+{
+	printf("block -> '{' decls stmts '}'\n");
+	if (lookahead == '{'){
+		match('{'); decls(); stmts();match('}');
+	}
+	else
+		error(2);
+}
+
+void decls()
+{
+    if (lookahead == INT || lookahead == FLOAT || lookahead == CHAR){
+        printf("decls -> decl decls\n");
+        decl(); decls();
+    }
+    else if (lookahead == ID || lookahead == IF 
+        || lookahead == WHILE || lookahead == DO 
+        || lookahead == BREAK || lookahead == '{'
+        || lookahead == '}'){
+        printf("decls -> epsilon\n");
+        ;
+    }
+    else
+        error(3);
+}
+
+void decl()
+{
+    if(lookahead == INT || lookahead == FLOAT) {
+        printf("decl -> type ID ';'\n");
+        type(); match(ID); match(';');
+    }
+    else
+        error(4);
+}
+
+void stmts()
+{
+	//5 stmts -> stmt stmts 
+	if (lookahead == ID || lookahead == IF 
+        || lookahead == WHILE || lookahead == DO 
+        || lookahead == BREAK || lookahead == '{'){
+		printf("stmts -> stmt stmts \n");
+		stmt(); stmts();
+	}
+	else if (lookahead == '}'){
+		printf("stmts -> epsilon \n");
+	}
+	else
+		error(5);
+	
+}
+
+
+void stmt()
+{
+	if (lookahead == ID){
+		printf("stmt -> ID '=' expr ';'\n");
+		match(ID); match('='); expr(); match(';');
+	}
+	else if (lookahead == IF){
+		printf("stmt -> IF (bool) stmt Elsestmt\n");
+		match(IF); match('('); Bool(); match(')'); stmt(); ElseStmt();
+	}
+    else if(lookahead == WHILE){
+        printf("stmt -> WHILE (bool) stmt\n");
+        match(WHILE); match('('); Bool(); match(')'); stmt();
+    }
+    else if(lookahead == DO){
+        printf("stmt -> DO stmt WHILE (bool) ';'\n");
+        match(DO); stmt(); match(WHILE); match('('); Bool(); match(')'); match(';');
+    }
+    else if(lookahead == BREAK){
+        printf("stmt -> break ';'\n");
+        match(BREAK); match(';');
+    }
+    else if(lookahead == '{'){
+        printf("stmt -> block\n");
+        block();
+    }
+	else
+		error(6);
+}
+
+void ElseStmt()
+{
+    if (lookahead == ELSE){
+        printf("ElseStmt -> ELSE stmt\n");
+        match(ELSE); stmt();
+    }
+    else if (lookahead == ID || lookahead == IF 
+        || lookahead == WHILE || lookahead == DO 
+        || lookahead == BREAK || lookahead == '{'
+        || lookahead == '}'){
+        printf("ElseStmt -> epsilon\n");
+        ;
+    }
+    else
+        error(7);
+}
+
+void type()
+{
+	if (lookahead == INT){
+		printf("type -> INT \n");
+		match(INT);
+	}
+	else if (lookahead == FLOAT){
+		printf("type -> FLOAT\n");
+		match(FLOAT);
+	}
+    else if (lookahead == CHAR){
+        printf("type -> CHAR\n");
+        match(CHAR);
+    }
+	else
+		error(8);
+} 
+
+void Bool()
+{
+    if (lookahead == ID || lookahead == NUM || lookahead == '('){
+        printf("bool -> expr boolR\n");
+        expr(); BoolR();
+    }
+    else
+        error(9);
+
+
+}
+
+void BoolR()
+{
+    if (lookahead == '<' || lookahead == '>' 
+        || lookahead == LE || lookahead == GE 
+        || lookahead == EQ || lookahead == NE){
+        printf("boolR -> < expr\n");
+        match(lookahead); expr();
+    }
+    else if (lookahead == ')' || lookahead == ';' || lookahead == '{' || lookahead == '}'){
+        printf("boolR -> epsilon\n");
+    }
+    else
+        error(10);
+}
+
+void expr()
+{
+    // printf("lookahead = %d\n", lookahead);
+    if (lookahead == ID || lookahead == NUM || lookahead == '('){
+        printf("expr -> term exprR\n");
+        term(); exprR();
+    }
+    else
+        error(11);
+}
+
+void exprR()
+{
+    if (lookahead == '+'){
+        printf("exprR -> + term exprR\n");
+        match(lookahead);term(); exprR();
+    }
+    else if (lookahead == '-'){
+        printf("exprR -> - term exprR\n");
+        match(lookahead);term(); exprR();
+    }
+    
+    else if (lookahead == ')' || lookahead == ';' || lookahead == '<' || lookahead == '>' 
+            || lookahead == LE || lookahead == GE || lookahead == EQ || lookahead == NE){
+        
+        printf("exprR -> epsilon\n");
+    }
+    else
+        error(12);
+}
+
+void term()
+{
+    if (lookahead == ID || lookahead == NUM || lookahead == '('){
+        printf("term -> factor termR\n");
+        factor(); termR();
+    }
+    else
+        error(13);
+}
+
+void termR()
+{
+    if (lookahead == '*'){
+        printf("termR -> * factor termR\n");
+        match(lookahead);factor(); termR();
+    }
+    else if (lookahead == '/'){
+        printf("termR -> / factor termR\n");
+        match(lookahead);factor(); termR();
+    }
+    else if (lookahead == ')' || lookahead == ';' || lookahead == '<' || lookahead == '>' 
+            || lookahead == LE || lookahead == GE || lookahead == EQ || lookahead == NE
+            || lookahead == '+' || lookahead == '-'){
+        printf("termR -> epsilon\n");
+    }
+    else
+        error(14);
+}
+
+void factor()
+{
+    if (lookahead == ID){
+        printf("factor -> ID\n");
+        match(ID);
+    }
+    else if (lookahead == NUM){
+        printf("factor -> NUM\n");
+        match(NUM);
+    }
+    else if (lookahead == '('){
+        printf("factor -> (expr)\n");
+        match('('); expr(); match(')');
+    }
+    else
+        error(15);
+}
+
+void Parse()
+{
+	lookahead = yylex();
+	program();  //start symbol
+}
+
+int main()
+{
+    // 支持从文件读取
+    char filename[1000] = "";
+    printf("Enter the filename: ");
+    scanf("%s", &filename);
+    printf("\n");
+    freopen(filename, "r", stdin);
+ 	Parse();
+    return 0;
+}
+
